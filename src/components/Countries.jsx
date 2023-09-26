d00b3f35a2e8db7f7a7e5baea396bcc16663d7b5
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-26 07:40:04 +0200

    add links to coutry card

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Filter from "./Filter";
 
 const url = "https://restcountries.com/v2/all";
@@ -24,23 +25,21 @@ const Countries = () => {
           const { numericCode, name, population, region, capital, flag } =
             country;
           return (
-            <article key={numericCode}>
-              <div>
-                <img src={flag} alt="name" />
-                <div className="details">
-                  <h3>{name}</h3>
-                  <h4>
-                    Population: <span>{population}</span>
-                  </h4>
-                  <h4>
-                    Region: <span>{region}</span>
-                  </h4>
-                  <h4>
-                    Capital: <span>{capital}</span>
-                  </h4>
-                </div>
+            <Link to={`/countries/${name}`} key={numericCode} className="link">
+              <img src={flag} alt="name" />
+              <div className="details">
+                <h3>{name}</h3>
+                <h4>
+                  Population: <span>{population}</span>
+                </h4>
+                <h4>
+                  Region: <span>{region}</span>
+                </h4>
+                <h4>
+                  Capital: <span>{capital}</span>
+                </h4>
               </div>
-            </article>
+            </Link>
           );
         })}
       </section>
