e5f5c98e510c81a3abced1d5260c31f637c8117f
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-26 08:20:13 +0200

    populate country page with info

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -21,6 +21,51 @@ const Country = () => {
         <FaArrowLeftLong />
         Back
       </Link>
+      <section className="country">
+        {country.map((c) => {
+          const {
+            numericCode,
+            flag,
+            name,
+            nativeName,
+            population,
+            region,
+            subregion,
+            capital,
+            topLevelDomain,
+            currencies,
+            languages,
+            border,
+          } = c;
+
+          return (
+            <article key={numericCode}>
+              <div className="flag">
+                <img src={flag} alt={name} />
+              </div>
+
+              <div className="country-details">
+                <div>
+                  <h2>{name}</h2>
+                  <h5>Native Name: {nativeName}</h5>
+                  <h5>Population: {population}</h5>
+                  <h5>Region: {region}</h5>
+                  <h5>Sub Region: {subregion}</h5>
+                  <h5>Capital: {capital}</h5>
+                </div>
+                <div>
+                  <h5>Top Level Domain: {topLevelDomain[0]}</h5>
+                  <h5>Currencies: {currencies[0].code}</h5>
+                  <h5>Languages {languages[0].name}</h5>
+                </div>
+              </div>
+              <div>
+                <h3>Border Countries: {border}</h3>
+              </div>
+            </article>
+          );
+        })}
+      </section>
     </>
   );
 };
