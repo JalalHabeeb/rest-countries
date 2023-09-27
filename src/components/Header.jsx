ef3b738025d6106f50b303fd68a6ae2b0121d306
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-27 21:31:42 +0200

    add dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaMoon } from "react-icons/fa6";
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const appClass = darkMode ? "dark-mode" : "light-mode";
+  document.body.className = appClass;
+
   return (
     <>
       <section className="header">
         <div>
           <h1>Where in the world?</h1>
         </div>
-        <div>
+        <div onClick={toggleDarkMode} className="dark-toggle">
           <FaMoon /> Dark mode
         </div>
       </section>
