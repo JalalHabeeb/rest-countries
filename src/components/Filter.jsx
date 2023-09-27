a928f0177a3946b1d6edf6da233d90e8d5e480d3
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-27 18:22:55 +0200

    replace America with Americas

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -80,7 +80,7 @@ const Filter = ({ onRegionChange }) => {
         >
           <option value="">Filter by region</option>
           <option value="Africa">Africa</option>
-          <option value="America">America</option>
+          <option value="Americas">Americas</option>
           <option value="Asia">Asia</option>
           <option value="Europe">Europe</option>
           <option value="Oceania">Oceania</option>
